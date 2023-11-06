ef7a67b7751f16c025400b5f0505a28d6d52095c
yoana <yewonyoana@gmail.com>
2023-11-06 23:32:11 +0900

    Updated Files

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,23 @@
-hello;
+// app.js ➡️ Front End에서 구동 됨
+
+// ⬇️ Can send/receive msgs to Back End
+// - "socket" represents a connection to the web server (vs. "socket" in server.js)
+// - To have access to homepage on phone, using "window.location.host" instead of "localhost:3000"
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+// ⬇️ console.log in browser inspection
+socket.addEventListener("open", () => {
+	console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+	console.log("New message:", message.data);
+});
+
+socket.addEventListener("close", () => {
+	console.log("Disconnected from Server ❌");
+});
+
+setTimeout(() => {
+	socket.send("hello from the browser!");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -1,12 +1,38 @@
+//환경 구축
+// server.js ➡️ Back End에서 구동 됨
+
+import http from "http";
+import WebSocket from "ws";
+// ⬇️ Express handles http
 import express from "express";
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
+// ⬇️ The file that is public to the user
 app.use("/public", express.static(__dirname + "/public"));
-
+// ⬇️ Sender sends a "get request" to the "/" and the "server responds" with a home.pug template (rendering home.pug when user goes to "/(homepage)")
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+// ⬇️  Getting access to server with express app to use WebSocket
+const server = http.createServer(app);
+// ⬇️ Creating a WebSocket server
+// - With "({ server })",  WebSocket server with start with http server to share the same port (3000) (BUT optional)
+const wss = new WebSocket.Server({ server });
+
+// ⬇️ Helps communicate (send/receive msgs) with Front End in real-time (console.log in terminal)
+// - "socket" represents the browser that connected
+wss.on("connection", (socket) => {
+	console.log("Connected to Browser ✅");
+	socket.on("close", () => console.log("Disconnected from Browser ❌"));
+	socket.on("message", (message) => {
+		console.log(message.toString("utf8"));
+	});
+	socket.send("hello!");
+});
+
+server.listen(3000, handleListen);
